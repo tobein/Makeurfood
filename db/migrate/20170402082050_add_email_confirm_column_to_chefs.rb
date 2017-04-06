5967a2469fd6bb452f9345507cf7373e124501b6
Amir Khan <mailtoamir60@gmail.com>
2017-04-06 20:25:21 +0500

    Delete 20170402082050_add_email_confirm_column_to_chefs.rb

--- db/migrate/20170402082050_add_email_confirm_column_to_chefs.rb
+++
@@ -1,6 +0,0 @@
-class AddEmailConfirmColumnToChefs < ActiveRecord::Migration[5.0]
-  def change
-    add_column :chefs, :email_confirmed, :boolean, :default => false
-    add_column :chefs, :confirm_token, :string
-  end
-end
