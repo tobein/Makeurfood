2746ec71b1f10fa23cdc5417268f170039fe323b
Amir Khan <mailtoamir60@gmail.com>
2017-04-06 20:31:48 +0500

    Update chef.rb

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,5 +1,4 @@
 class Chef < ActiveRecord::Base
-  before_create :confirmation_token
   has_many :recipes
   has_many :likes
   before_save { self.email = email.downcase }
@@ -9,18 +8,5 @@ class Chef < ActiveRecord::Base
                                     uniqueness: { case_sensitive: false },
                                     format: { with: VALID_EMAIL_REGEX }
   has_secure_password
-
-  def email_activate
-    self.email_confirmed = true
-    self.confirm_token = nil
-    save!(:validate => false)
-  end
-
-  private
-    def confirmation_token
-      if self.confirm_token.blank?
-          self.confirm_token = SecureRandom.urlsafe_base64.to_s
-      end
-    end
 
 end
