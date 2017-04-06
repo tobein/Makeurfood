8be982c8a7aa6f44a419cf7935c0a3b7d7260d85
Amir Khan <mailtoamir60@gmail.com>
2017-04-06 20:32:39 +0500

    Delete chef_mailer.rb

--- app/mailers/chef_mailer.rb
+++
@@ -1,9 +0,0 @@
-class ChefMailer < ActionMailer::Base
-    default :from => "admin@makeurfood.com"
-
- def registration_confirmation(chef)chef
-    @chef = chef
-    mail(:to => "#{chef.chefname} <#{chef.email}>", :subject => "Registration Confirmation")
- end
-
- end
