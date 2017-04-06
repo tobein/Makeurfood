f4655c91c8875bf27ed4b9cfb9a100511bc3c375
Amir Khan <mailtoamir60@gmail.com>
2017-04-06 20:29:01 +0500

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,17 +28,6 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = {:host => "makeurfoods.herokuapp.com"}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'gmail.com',
-    chef_name: 'youremail@domain.com',
-    password: 'youremailpassword',
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
 
   config.action_mailer.perform_caching = false
 
